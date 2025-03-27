72beddbdd85e78b084c17ba3f8d1f1ea18a3830f
Mradul Raj Singh <mradulrajsingh61@gmail.com>
2025-03-27 23:06:29 +0530

    <did some other things lost track myself>

--- app.js
+++ app.js
@@ -1,21 +1,21 @@
 const express = require("express");
 const path = require("path");
+const fs = require("fs");
 const app = express();
 const port = 3000;
 
-// this is letting me use the static file 
-
 //1. This is for the main page
 app.use(express.static(path.join(__dirname, "public")));
-//-----
-
-//2.This is for the kana page
-app.use(express.static(path.join(__dirname,"/sections/kana")));
 
 // This serves the main page "GET" request
 app.get("/", (req,res) => {
     res.sendFile(__dirname+"/public/index.html");
 });
+//-----
+
+
+//2.This is for the kana page
+app.use(express.static(path.join(__dirname,"/sections/kana")));
 
 // This serves the kana page "GET" request
 app.get("/kana",(req,res) =>{
@@ -26,6 +26,31 @@ app.get("/kana",(req,res) =>{
 app.get("/api/kana", (req, res) => {
     res.sendFile(path.join(__dirname, "/data/kana.json"));
 });
+// this is the get function to fetch the svg data to show in the box
+app.get("/api/assets/kanji/:hex", (req, res) => {
+    const hexCode = req.params.hex.toLowerCase(); // Ensure uppercase
+    console.log(hexCode);
+    const svgPath = path.join(__dirname, "assets", "kanji", `${hexCode}.svg`);
+    console.log(svgPath);
+    // Check if the file exists
+    fs.readFile(svgPath, "utf8", (err, data) => {
+        if (err) {
+            return res.status(404).send("SVG not found");
+        }
+        res.setHeader("Content-Type", "image/svg+xml");
+        res.send(data);
+    });
+});
+//-----
+
+//3. This is for the kanji page
+app.use(express.static(path.join(__dirname,"/sections/kanji")));
+
+app.get("/kanji", (req,res) => {
+    res.sendFile(__dirname+"/sections/kanji/kanji.html");
+});
+//-----
+
 
 app.listen(port, () =>{
     console.log("server started at port: "+ port);
